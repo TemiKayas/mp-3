a4ef3ec0944600496c3400974b4b75d5bc948c8e
TemiKayas <artemioskayas@gmail.com>
2024-10-22 16:47:48 -0400

    Updated links to images to be absolute paths

--- src/Components/Hackathons.tsx
+++ src/Components/Hackathons.tsx
@@ -6,7 +6,7 @@ export default function Hackathons() {
             <h1>Hackathons</h1>
             <SmallDiv>
                 <h3>Boston Hacks 2023</h3>
-                <img src="../public/bh2023.jpg" alt="Photo of me at Boston Hacks in 2023"/>
+                <img src="/public/bh2023.jpg" alt="Photo of me at Boston Hacks in 2023"/>
 
                 <p>The photo above is of my team and I presenting our project for Boston Hacks 2023. We build
                     a ChatGPT based app integrated with the capital one API to provide financial advice. Was a fun
@@ -14,7 +14,7 @@ export default function Hackathons() {
             </SmallDiv>
             <SmallDiv>
                 <h3>WHACK 2023</h3>
-                <img src="../public/whack2023.JPG" alt="Photo of my group and I at WHACK 2023"/>
+                <img src="/public/whack2023.JPG" alt="Photo of my group and I at WHACK 2023"/>
 
                 <p>This was a photo I took at WHACK2023, it depicts my group and I enjoying lunch at a picnic
                     bench on a cool November day. We build a project with a focus on tracking and deploying drones.
@@ -22,7 +22,7 @@ export default function Hackathons() {
             </SmallDiv>
             <SmallDiv>
                 <h3>WHACK 2022</h3>
-                <img src="../public/whack2022.JPG" alt="Photo of my group and I at WHACK 2022"/>
+                <img src="/public/whack2022.JPG" alt="Photo of my group and I at WHACK 2022"/>
 
                 <p>This was my very fist Hackathon! I went to Wellesley College's hackathon by my self and
                     matched up on a team with people I met that were looking for a group. We build an app that
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,19 +1,19 @@
 import {BigDiv, SmallDiv} from "./ComponentStyles.tsx";
-
+import {Link} from "react-router-dom";
 export default function Projects() {
     return (
         <BigDiv>
             <h1>Projects</h1>
             <SmallDiv>
                 <h2>FPV Drone</h2>
-                <img src="../public/drone.JPG" alt="Photo of the FPV racing drone I build"/>
+                <img src="/public/drone.JPG" alt="Photo of the FPV racing drone I build"/>
                 <p>This is an image of an FPV racing drone I built and programmed last summer. Of all of the
                     projects I have done, this has got to be one of my favorites. In my opinion, FPV drone flying
                     is the closest am average person can get to flying a fighter jet.</p>
             </SmallDiv>
             <SmallDiv>
             <h2>To see my other projects...</h2>
-                <p>To see my software projects, please check my <a href={"https://github.com/TemiKayas"}>Github</a>. My GitHub page is where I upload and document all of my software projects.</p>
+                <p>To see my software projects, please check my <Link to={"https://github.com/TemiKayas"}>Github</Link>. My GitHub page is where I upload and document all of my software projects.</p>
             </SmallDiv>
         </BigDiv>
     );
